7e465a30e502cbafb7201d88ccf91c572ae4c11e
Geovana <107410963+gigixw@users.noreply.github.com>
2024-10-25 11:42:43 -0300

    Add files via upload

---
+++ InformacoesGlobais.js
@@ -0,0 +1,10 @@
+const url = "https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json"
+
+async function
+visualizarinformacoesGlobais() {
+    const res = await fetch(url);
+    const dados = await res.json();
+    console.log(dados);
+}
+
+visualizarinformacoesGlobais()
