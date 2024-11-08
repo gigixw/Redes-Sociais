cd036e11291d26f292c5642ee5f193b08e92b828
Geovana <107410963+gigixw@users.noreply.github.com>
2024-11-08 11:44:26 -0300

    Add files via upload

--- informacoesGlobais.js
+++ informacoesGlobais.js
@@ -7,6 +7,7 @@ async function vizualizarInformacoesGlobais() {
     const pessoasNoMundo = (dados.total_pessoas_mundo) / 1e9
     const horas = parseInt(dados.tempo_medio)
     const minutos = Math.round ((dados.tempo_medio - horas)* 100)
+    const porcentagemConectada = ((pessoasConectadas / pessoasNoMundo) * 100).toFixed(2)
 
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
@@ -16,7 +17,9 @@ async function vizualizarInformacoesGlobais() {
     que aproximadamente <span>${pessoasConectadas} bilhões 
     </span> estão conectadas em alguma rede social e passam 
     em média <span>${horas}</span> horas e 
-    <span>${minutos}</span> minutos conectadas.`
+    <span>${minutos}</span> minutos conectadas.
+    <br> Isso significa que aproximadamente <span>${porcentagemConectada}%</span>
+    de pessoas estão conectadas em alguma rede social.`
     
     const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo)
